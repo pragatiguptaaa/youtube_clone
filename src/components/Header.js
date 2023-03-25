77944033a9d896c9f5d13dd5fb9e38132847d296
Pragati Gupta <pragatigupta989@gmail.com>
2023-03-25 19:49:57 +0530

    Getting suggestions on every key press in search input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 
 import HamBurger from '../imgs/HamBurger.png';
 import {toggleMenu} from '../utils/menuSlice';
 import YouTubeLogo from '../imgs/YouTubeLogo.png';
 import ProfileLogo from '../imgs/ProfileLogo.jpg';
-
+import { YOUTUBE_SEARCH_API } from '../utils/constants';
 
 //TOOD: Findout why h-8 works for img and not div.
 const Header = () =>{
+  const [searchQuery, setSearchQuery] = useState("");
   const dispatch = useDispatch();
 
   const toggleMenuHandler = () =>{
     dispatch(toggleMenu());
   }
+
+  async function getSuggestions() {
+     const data = await fetch(YOUTUBE_SEARCH_API+searchQuery);
+     const json = data.json();
+  }
   
+  useEffect( () => {
+    getSuggestions();
+  }, [searchQuery]);
+
+
   return (
     <div className='grid grid-flow-col p-5 m-2 shadow'>
-        <div className = 'col-span-2 flex'>
+        <div className = 'col-span-3 flex'>
           <img className='h-10'
-           alt = "Loading Menu Icon..." 
+            alt = "Loading Menu Icon..." 
             src = {HamBurger} 
             onClick ={()=> toggleMenuHandler()}
           />
@@ -28,11 +39,16 @@ const Header = () =>{
             src = {YouTubeLogo}
           />
         </div>
-        <div className='col-span-10 flex justify-center'>
-          <input type="text" className='w-1/2 h-10 border border-gray-800   rounded-l'/>
-          <button className='h-10  bg-gray-800 text-white rounded-r'> Search</button>
+        <div className='mx-10 px-10 col-span-6 flex justify-center'>
+          <input 
+            type="text" 
+            className='w-10/12 h-10 border border-gray-800 rounded-l'
+            value = {searchQuery}
+            onChange = {(e) => setSearchQuery(e.target.value)}
+          />
+          <button className='w-2/12 h-10  bg-gray-800 text-white rounded-r'> Search </button>
         </div>
-        <div className='col-span-2'>
+        <div className='col-span-3'>
           <img className='h-10'
             alt="Loading Profile Icon..."
             src={ProfileLogo}
